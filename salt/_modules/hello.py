45ba761ebac1c4d9701f4d91abc8f15962cf27a1
Gareth J. Greenaway <gareth@wiked.org>
2018-05-06 17:37:46 -0700

    Moving _modules under salt

---
+++ salt/_modules/hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+A simple module to give a greeting
+'''
+
+from __future__ import absolute_import
+
+
+def world():
+    '''
+    Say Hello
+    '''
+    return "Hello World!"
