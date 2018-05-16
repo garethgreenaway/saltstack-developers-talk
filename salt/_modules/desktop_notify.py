a3bb16888413d2ec96c37ff79523e2f3880fa96f
Gareth J. Greenaway <gareth@wiked.org>
2018-05-15 20:06:32 -0700

    Fixing __future__ import

--- salt/_modules/desktop_notify.py
+++ salt/_modules/desktop_notify.py
@@ -4,7 +4,7 @@ Desktop Notification
 '''
 
 # Import Python libs
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals, print_function
 import logging
 import os
 import pwd
--- salt/_modules/hello.py
+++ salt/_modules/hello.py
@@ -3,7 +3,7 @@
 A simple module to give a greeting
 '''
 
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals, print_function
 
 
 def world():
